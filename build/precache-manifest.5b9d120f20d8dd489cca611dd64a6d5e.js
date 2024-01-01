6f6304283b550baf87e9151e89d73a17407261d5
amansingh4u98 <amansingh199835@gmail.com>
2024-01-01 22:38:23 +0530

    build folder for the frontend

---
+++ build/precache-manifest.5b9d120f20d8dd489cca611dd64a6d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c0c2fcdce1fa3b8fd3190608580774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b8e605a9dbc9e0f15e",
+    "url": "/static/css/main.8e0bc7ef.chunk.css"
+  },
+  {
+    "revision": "ad75e58f81646ffdecc1",
+    "url": "/static/js/2.40bf92fb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.40bf92fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca96402b3e23843231b4",
+    "url": "/static/js/3.07b2ed7b.chunk.js"
+  },
+  {
+    "revision": "60b8e605a9dbc9e0f15e",
+    "url": "/static/js/main.3d455f24.chunk.js"
+  },
+  {
+    "revision": "5ee1be775351ecc7f146",
+    "url": "/static/js/runtime-main.83d3cd55.js"
+  }
+]);
